86bedd902033a4ee6acefc970246cd14f51a8c5b
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2016-03-29 01:19:48 -0300

    creating sample

---
+++ app/src/main/java/com/github/rodrigohenriques/bottombarsample/MainActivity.java
@@ -0,0 +1,69 @@
+package com.github.rodrigohenriques.bottombarsample;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarBadge;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomBar mBottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user selected item number one.
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user reselected item number one, scroll your content to top.
+                }
+            }
+        });
+
+        // Setting colors for different tabs when there's more than three of them.
+        // You can set colors for tabs in three different ways as shown below.
+        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+        mBottomBar.mapColorForTab(1, 0xFF5D4037);
+        mBottomBar.mapColorForTab(2, "#7B1FA2");
+        mBottomBar.mapColorForTab(3, "#0044FF");
+
+        BottomBarBadge unreadMessages = mBottomBar.makeBadgeForTabAt(0, "#FF0000", 13);
+
+        // Control the badge's visibility
+        unreadMessages.show();
+        //unreadMessages.hide();
+
+        // Change the displayed count for this badge.
+        //unreadMessages.setCount(4);
+
+        // Change the show / hide animation duration.
+        unreadMessages.setAnimationDuration(200);
+
+        // If you want the badge be shown always after unselecting the tab that contains it.
+        unreadMessages.setAutoShowAfterUnSelection(true);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
+    }
+}
